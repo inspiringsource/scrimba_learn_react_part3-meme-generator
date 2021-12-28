852e37811f23d6fb7dd43a4f1627384b692c805b
inspiringsource <ab.b2k3@gmail.com>
2021-12-28 16:40:11 +0100

    forms 👍

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,21 +1,22 @@
 import React from "react"
 
-export default function Form(props) {
+export default function Form() {
     const [formData, setFormData] = React.useState(
-        {firstName: "", lastName: ""}
+        {firstName: "", lastName: "", email: "", comments: "", isFavorite: false}
     )
     
     console.log(formData)
     
     function handleChange(event) {
+        const {name, value, type, checked} = event.target
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
-                [event.target.name]: event.target.value
+                [name]: type=== "checkbox" ? checked : value
             }
         })
     }
-    
+    let checkBox = formData.isFavorite ? "checked" : "Not checked"
     return (
         <form>
             <input
@@ -23,18 +24,41 @@ export default function Form(props) {
                 placeholder="First Name"
                 onChange={handleChange}
                 name="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
                 name="lastName"
+                value={formData.lastName}
             />
+            <input
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+            />
+            <textarea placeholder="Comments" onChange={handleChange} name="comments" value={formData.comments} />
+            
+            <input 
+            type="checkbox" 
+            value={formData.isFavorite} 
+            checked={formData.isFriendly}
+            onChange={handleChange}
+            name="isFavorite" 
+            />
+        
         <div className="outPut">
             <p>You typed: </p>
             <p>Firstname: {formData["firstName"]}</p>
             <p>Lastname: {formData["lastName"]}</p>
+            <p>Email: {formData["email"]}</p>
+            <p>Comments: {formData["comments"]}</p>
+            {checkBox &&<p>isFavorite: {checkBox}</p>}
         </div>
         </form>
     )
 }
+//full from template on github
